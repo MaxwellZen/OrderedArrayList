19e99c6d420ed9abbc6894b36a54ed88dd79c7d9
MaxwellZen <>
2020-12-11 01:31:49 -0500

    wrote a tester, tested both classes, and fixed the index helper method

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.io.*;
-public class OrderedArrayList<T> implements Comparable<T> extends NoNullArrayList<T> {
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
   public OrderedArrayList() {
     super();
   }
@@ -8,20 +8,26 @@ public class OrderedArrayList<T> implements Comparable<T> extends NoNullArrayLis
     super(capacity);
   }
   private int index(T element) {
-    int ans = 0;
-    while (element.compareTo(super.get(ans))<0) {
-      ans += 1;
+    if (super.size()==0) return 0;
+    if (element.compareTo(super.get(0))<0) return 0;
+    int ans = 1;
+    while (ans < super.size()-1 && element.compareTo(super.get(ans))>0) {
+      ans++;
     }
+    if (element.compareTo(super.get(super.size()-1))>0) return super.size();
     return ans;
   }
   public boolean add(T element) {
+    int check = super.size();
     super.add(index(element), element);
+    return super.size() == check+1;
   }
   public void add(int index, T element) {
     super.add(index(element), element);
   }
   public T set (int index, T element) {
-    super.remove(index);
+    T a = super.remove(index);
     super.add(index(element), element);
+    return a;
   }
 }
---
+++ tester.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+public class tester {
+  public static void main (String[] args) {
+    Integer a = 5;
+    Integer b = 3;
+    System.out.println(a.compareTo(b));
+    System.out.println("testing nonullarraylist:");
+    NoNullArrayList<Integer> test1 = new NoNullArrayList<Integer>();
+    NoNullArrayList<Integer> test2 = new NoNullArrayList<Integer>(50);
+    try {
+      test1.add(5);
+      test1.add(10);
+      test1.add(2);
+      test1.add(null);
+      test1.add(3);
+    } catch (IllegalArgumentException e){
+      e.printStackTrace();
+      for (Integer i : test1) {
+        System.out.print(i + " ");
+      }
+    }
+    System.out.println();
+    try {
+      test2.add(0, 5);
+      test2.add(1, 10);
+      test2.add(0, 2);
+      test2.add(1, null);
+      test2.add(1, 3);
+    } catch (IllegalArgumentException e) {
+      e.printStackTrace();
+      for (Integer i : test2) {
+        System.out.print(i+" ");
+      }
+    }
+    System.out.println();
+    OrderedArrayList<Integer> test = new OrderedArrayList<Integer>();
+    for (Integer i = 0; i < 10; i += 2) {
+      test.add(i);
+    }
+    for (Integer i = 1; i < 10; i += 2) {
+      test.add (i);
+    }
+    for (int i = 0; i < test.size(); i++) {
+      System.out.print(test.get(i) + " ");
+    }
+    System.out.println();
+  }
+}
